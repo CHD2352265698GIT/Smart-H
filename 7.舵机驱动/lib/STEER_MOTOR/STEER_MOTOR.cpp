5cb0330f67f542ea593901c524f9dd26bf81b05d
CHD <2352265698+Dvchen@user.noreply.gitee.com>
2024-07-23 01:02:54 +0800

    添加舵机驱动，修改网页，局域网版通信

---
+++ 7.舵机驱动/lib/STEER_MOTOR/STEER_MOTOR.cpp
@@ -0,0 +1,3 @@
+#include <STEER_MOTOR.h>
+#include <Ticker.h>
+Servo myservo; // 声明servo对象
---
+++ 7.舵机驱动/src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WebServer.h>
+#include <LED.h>
+#include <WIFI_STA_AP.h>
+#include <STEER_MOTOR.h>
+#include <PubSubClient.h>
+#include <MQTT.h>
+#include <Ticker.h>
+
+WIFI_STA_AP WIFI; // 实例化WIFI_STA_AP类
+MQTT *aliyu;
+
+void setup()
+{
+  p_WIFI_STA_AP = &WIFI;              // 保存WIFI_STA_AP类的指针
+  Serial.begin(115200);               // 设置串口波特率
+  WIFI.initSoftAP_STA();              // 初始化WIFI SoftAP模式
+  WIFI.initWebServer();               // 初始化WIFI WebServer
+  pinMode(LED_PIN, OUTPUT);           // 设置GPIO模式为输出
+  WIFI.Read_WIFI_STA_AP_Config();     // 读取flash保存的wifi信息
+  WiFi.begin(sta_ssid, sta_password); // 连接上一次连接成功的wifi
+  digitalWrite(LED_PIN, HIGH);        // 关闭LED
+}
+
+void loop()
+{
+  esp8266_server.handleClient(); // 处理http服务器访问
+}
